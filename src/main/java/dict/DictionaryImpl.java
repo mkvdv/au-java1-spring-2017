dd0e384532a722a5cfc6e3a39693d43420c36c04
avallon <makeevvolodja@gmail.com>
2018-03-22 20:09:21 +0300

    implemented almost all

--- src/main/java/dict/DictionaryImpl.java
+++ src/main/java/dict/DictionaryImpl.java
@@ -7,14 +7,23 @@ import java.util.LinkedList;
 
 public class DictionaryImpl implements Dictionary {
     private final int maxChainLength;
+    private final double MIN_FILL_FACTOR = 0.1;
+    private final int DEFAULT_MAX_SIZE = 64;
+
+    private int maxSize = DEFAULT_MAX_SIZE; // 2 ** 7
     private int size = 0;
-    private ArrayList<LinkedList<String>> table;
+    private ArrayList<LinkedList<Node>> table = new ArrayList<>(maxSize);
 
     DictionaryImpl(int maxChainLength) {
         if (maxChainLength < 0) {
             throw new IllegalArgumentException();
         }
         this.maxChainLength = maxChainLength;
+
+        for (int i = 0; i < maxSize; i++) {
+            table.add(new LinkedList<>());
+        }
+
     }
 
     @Override
@@ -24,30 +33,130 @@ public class DictionaryImpl implements Dictionary {
 
     @Override
     public boolean contains(@NotNull String key) {
-        // todo impl
-        return false;
+        int list_ix = myHash(key);
+        return find_key(table.get(list_ix), key) != -1;
     }
 
     @Override
     public String get(@NotNull String key) {
-        // todo impl
-        return null;
+        int listIx = myHash(key);
+        int stringIx = find_key(table.get(listIx), key);
+
+        if (stringIx != -1) {
+            return table.get(listIx).get(stringIx).value;
+        } else {
+            return null;
+        }
     }
 
     @Override
     public String put(@NotNull String key, @NotNull String value) {
-        // todo impl
-        return null;
+        LinkedList<Node> chain = table.get(myHash(key));
+        int string_ix = find_key(chain, key);
+
+        String prev = null;
+        if (string_ix != -1) { // contains
+            prev = chain.get(string_ix).value;
+            chain.set(string_ix, new Node(key, value));
+        } else {
+            if (chain.size() < maxChainLength && size < maxSize) {
+                chain.add(new Node(key, value));
+            } else {
+                rehash(size * 2); // increase size
+                chain = table.get(myHash(key));
+                chain.add(new Node(key, value));
+            }
+            size++;
+        }
+
+        return prev;
     }
 
     @Override
     public String remove(@NotNull String key) {
-        // todo impl
-        return null;
+        LinkedList<Node> chain = table.get(myHash(key));
+        int string_ix = find_key(chain, key);
+
+        String value = null;
+        if (string_ix != -1) { // contains
+            value = chain.get(string_ix).value; // save for returning
+
+            if ((double) size / maxSize < MIN_FILL_FACTOR) {
+                rehash(size / 2); // reduce size
+            }
+            chain = table.get(myHash(key));
+            chain.remove(string_ix);
+            size--;
+        }
+
+        return value;
+    }
+
+    private void rehash(int new_max_size) {
+        assert new_max_size > 0;
+        System.out.println("Rehash with new size = " + new_max_size);
+        maxSize = new_max_size; // so hash will be another
+        ArrayList<LinkedList<Node>> new_table = new ArrayList<>(new_max_size);
+
+        for (int i = 0; i < maxSize; i++) {
+            new_table.add(new LinkedList<>());
+        }
+
+        for (LinkedList<Node> old_chain : table) {
+            for (Node old_node : old_chain) {
+                LinkedList<Node> new_chain = new_table.get(myHash(old_node.key)); // new hash
+                new_chain.add(old_node);
+            }
+        }
+
+        table = new_table;
+    }
+
+    // return ix of node with this key in chain, else -1 if not exist
+    private int find_key(LinkedList<Node> chain, String key) {
+        for (int ix = 0; ix < chain.size(); ix++) {
+            Node node = chain.get(ix);
+            if (node.equals(key)) {
+                return ix;
+            }
+        }
+
+        return -1; // not found
     }
 
     @Override
     public void clear() {
-        // todo impl
+        table.clear();
     }
+
+    private int myHash(@NotNull String key) {
+        return key.hashCode() % maxSize;
+    }
+
+    private static class Node {
+        String key;
+        String value;
+
+        Node(@NotNull String key, @NotNull String value) {
+            this.key = key;
+            this.value = value;
+        }
+
+        public int hashCode() {
+            return key.hashCode();
+        }
+
+        public boolean equals(@NotNull Node other) {
+            return this.key.equals(other.key);
+        }
+
+        public boolean equals(@NotNull String other_key) {
+            return this.key.equals(other_key);
+        }
+
+        public String toString() {
+            return "(" + key + ", " + value + ")";
+        }
+    }
+
 }
--- src/test/java/dict/DictionaryImplTest.java
+++ src/test/java/dict/DictionaryImplTest.java
@@ -2,15 +2,47 @@ package dict;
 
 import org.junit.Test;
 
+import static org.junit.Assert.*;
+
 public class DictionaryImplTest {
 
-    private static DictionaryImpl instance(int maxChainLength) {
-        return new DictionaryImpl(maxChainLength);
+    private static DictionaryImpl instance() {
+        return new DictionaryImpl(10);
     }
 
     @Test
-    public void test() {
-        // todo impl
+    public void test1() {
+        Dictionary d = instance();
+        final int size = 100;
+
+        for (int i = 0; i < size; i++) {
+            assertEquals(null, d.put(Integer.toString(i), Integer.toString(1000 + i)));
+        }
+
+        for (int i = 0; i < size; i++) {
+            assertNotEquals(null, d.put(Integer.toString(i), Integer.toString(1000 + i)));
+        }
+
+        for (int i = 0; i < size; i++) {
+            assertEquals(Integer.toString(1000 + i), d.get(Integer.toString(i)));
+        }
+
+        for (int i = size; i < 2 * size; i++) {
+            assertEquals(null, d.get(Integer.toString(i)));
+        }
+
+        assertEquals(d.size(), size);
+
+        for (int i = 0; i < size; i++) {
+            assertTrue(d.contains(Integer.toString(i)));
+        }
+
+        for (int i = size / 20; i < size; ++i) {
+            assertNotEquals(null, d.remove(Integer.toString(i)));
+        }
+        assertEquals(size / 20, d.size());
+
+        d.clear();
     }
 
 }
